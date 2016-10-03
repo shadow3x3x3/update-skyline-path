1d0405bbff32acfe8d9f9b83656b8d5577f8f259
sheng wei qiu <shadow3x3x3@gmail.com>
2016-10-03 15:41:06 +0800

    prevent inner nodes get duplicate nodes

--- growing_graph.rb
+++ growing_graph.rb
@@ -1,10 +1,12 @@
+require 'pry'
+
 class GrowingGraph
   attr_reader :outer_nodes, :inner_nodes
 
   def initialize(neighbors_hash, start_nodes)
     @neighbors_hash = neighbors_hash
 
-    @outer_nodes = [start_nodes]
+    @outer_nodes = start_nodes
     @inner_nodes = []
   end
 
@@ -32,9 +34,45 @@ class GrowingGraph
 
   def check_partial_outer_nodes(new_partial_outer_nodes, o_n)
     unless new_partial_outer_nodes.empty?
-      @inner_nodes << o_n
+      @inner_nodes << o_n unless @inner_nodes.include?(o_n)
       return new_partial_outer_nodes
     end
     o_n
   end
 end
+
+NeighborsHash = {
+  1  => [2, 3, 4, 5, 14],
+  2  => [1, 6, 7, 13],
+  3  => [1, 11, 12, 13],
+  4  => [1, 5, 8],
+  5  => [1, 4, 9, 10],
+  6  => [2],
+  7  => [2],
+  8  => [4],
+  9  => [5],
+  10 => [5],
+  11 => [3],
+  12 => [3],
+  13 => [2, 3],
+  14 => [1]                  
+}
+
+gg = GrowingGraph.new(NeighborsHash, [1, 2])
+puts gg.outer_nodes
+puts "--"
+puts gg.inner_nodes
+puts "--"
+puts '1-G'
+gg.growing
+puts gg.outer_nodes
+puts "--"
+puts gg.inner_nodes
+puts "--"
+puts '2-G'
+gg.growing
+puts gg.outer_nodes
+puts "--"
+puts gg.inner_nodes
+puts "--"
+
